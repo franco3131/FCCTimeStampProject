ef0c7a1d97b0976c22999583d606abde4431e4d3
David Franco <franco3131@yahoo.com>
2022-07-19 01:09:05 -0700

    remove commented lines

--- boilerplate-project-timestamp/index.js
+++ boilerplate-project-timestamp/index.js
@@ -41,65 +41,6 @@ app.get("/api/:date?", function(req, res)
 
 
 
-
-
-
-
-
-
-
-
-  
-
-  
-
-
-
-
-
-
-
-
-
-  
-//   var dateString = req.params.date;
-//   // if(Date.parse(dateString)!=NaN){
-// var timeZone="";
-//         // if(Number.isInteger(parseInt(dateString,10))===true){
-        //   console.log("hhhheeer");
-        //   var milliseconds = dateString/10;     
-          
-          //1451001600000
-          
-          // var dateObject = new Date(milliseconds);
-          // console.log(dateObject.toLocaleString());
-          // var year=dateObject.toLocaleString("en-US", {year: "numeric"});
-          // console.log(year);
-          // var month=dateObject.toLocaleString("en-US",            {month: "long"}) ;
-          // var day=dateObject.toLocaleString("en-US",               {day: "numeric"});
-          //   dateString=year+"/"+month+"/"+day+" "+ dateObject.getTimezoneOffset();
-   
-       
-      
-            // }
-  //   console.log(dateString);
-  // var dt = new Date(dateString);
-//   var wordDay=dt.toLocaleDateString('en', { weekday: 'long' });
-// //unix":1451001600000,"utc":"Fri, 25 Dec 2015 00:00:00 GMT"
-// var month=dt.toLocaleString('en', { month: 'short' })
-//   var responseString =wordDay+", "+ dt.getDate()+" "+month+" "+dt.getFullYear()+" "+ +dt.getTimezoneOffset();
-  //   var responseString=dt.toUTCString();
-  //   console.log(responseString);
-  // var unix=(new Date(dateString).valueOf());
-  // if(responseString==="Invalid Date"){
-  //     res.json({ error:"Invalid Date"});
-  // }else{
-  // res.json({ unix:unix, utc:responseString});
-  // }
-  
-  // }});
-
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
